439e44ee991d16625c226b3caa4b585e3e8dbe05
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-09 23:04:37 -0500

    Add files via upload

---
+++ favorite_numbers.py
@@ -0,0 +1,27 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+favorite_numbers = {
+	'derek': 24,
+	'daniel': 23,
+	'roger': 12,
+	'mike': 22,
+	'jr': 8,
+	}
+
+num = favorite_numbers['derek']
+print("Derek's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['daniel']
+print("Daniel's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['roger']
+print("Roger's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['mike']
+print("Mike's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['jr']
+print("JR's favorite number is " + str(num) + ".")
+
