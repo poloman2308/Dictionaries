8289301a374e17babb456906246809db12d89769
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-09 22:56:51 -0500

    Add files via upload

---
+++ person.py
@@ -0,0 +1,16 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+person = {
+	'first_name': 'derek',
+	'last_name': 'acevedo',
+	'age': 32,
+	'city': 'abilene',
+	}
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
+
