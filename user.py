4e0079326ec618a3e749079499e533cd8c1fcb5b
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-11 22:47:29 -0500

    Add files via upload

---
+++ user.py
@@ -0,0 +1,14 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+user_0 = {
+	'username': 'dacevedo',
+	'first': 'derek',
+	'last': 'acevedo',
+	}
+
+for key, value in user_0.items():
+	print("\nKey: " + key)
+	print("Value: " + value)
+
