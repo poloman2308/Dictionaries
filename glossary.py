367fea755c5efc4844257064ea005e1325b051c0
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-11 22:41:13 -0500

    Add files via upload

---
+++ glossary.py
@@ -0,0 +1,27 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+glossary = {
+	'string': 'A series of characters',
+	'comment': 'A note in a program that the Python interpreter ignores',
+	'list': 'A collection of items in a particular order',
+	'loop': 'Work through a collection of items, on at a time',
+	'dictionary': 'A collection of key-value pairs',
+	}
+
+word = 'string'
+print("\n" + word.title() + ": " + glossary[word] + ".")
+
+word = 'comment'
+print("\n" + word.title() + ": " + glossary[word] + ".")
+
+word = 'list'
+print("\n" + word.title() + ": " + glossary[word] + ".")
+
+word = 'loop'
+print("\n" + word.title() + ": " + glossary[word] + ".")
+
+word = 'dictionary'
+print("\n" + word.title() + ": " + glossary[word] + ".")
+
