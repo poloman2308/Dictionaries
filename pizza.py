1ee5dacec310596d8d4e655728416b17fcd7cb8a
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-13 22:42:34 -0500

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,16 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+# Store information about a pizza being ordered.
+pizza = {
+	'crust': 'thick',
+	'toppings': ['mushrooms', 'extra cheese'],
+	}
+
+# Summarize the order.
+print("You ordered a " + pizza['crust'] + "-crust pizza " + "with the following toppings:")
+
+for topping in pizza['toppings']:
+	print("- " + topping)
+
