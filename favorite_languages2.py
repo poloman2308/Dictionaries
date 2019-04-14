a5cc74715875fbb929831c21e090dc07e505cd75
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-13 22:47:53 -0500

    Add files via upload

---
+++ favorite_languages2.py
@@ -0,0 +1,16 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+favorite_languages = {
+	'jen': ['python', 'ruby'],
+	'sarah': ['c'],
+	'edward': ['ruby', 'go'],
+	'phil': ['python', 'haskell'],
+	}
+
+for name, languages in favorite_languages.items():
+	print("\n" + name.title() + "'s favorite languages are:")
+	for language in languages:
+		print("- " + language.title())
+
